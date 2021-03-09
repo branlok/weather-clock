e45e861f38ca1aaabcfcccea1ed0ee2b7a966a46
Brandon Lok <brandonlok@Brandons-MacBook-Pro.local>
2021-03-08 20:13:48 -0500

    vercel prod

--- components/BackgroundSlides.js
+++ components/BackgroundSlides.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { StyledBGContainer } from "../styles/StyledBGContainer";
-import {loadingImages} from "../store/general/generalActions";
-import {connect} from 'react-redux';
-import {StyledLoadingScreen} from '../styles/StyledLoadingScreen';
+import { loadingImages } from "../store/general/generalActions";
+import { connect } from "react-redux";
+import { StyledLoadingScreen } from "../styles/StyledLoadingScreen";
 import { useQueryClient } from "react-query";
 
 function BackgroundSlides(props) {
@@ -10,50 +10,72 @@ function BackgroundSlides(props) {
   const { data, status } = queryClient.getQueryState("images");
   console.log(status);
 
-  const [imageArray, setImageArray] = useState(false);
+  const [imgsLoaded, setImgsLoaded] = useState(false)
+//   useEffect(() => {
+//     if (status === "success") {
+//       setImageArray(;
+//     }
+//   }, [status]);
 
-  useEffect(() => {
-    if (status === "success") {
-      setImageArray(arrangeImage(data.unsplashData));
-      props.loadingImages();
+useEffect(() => {
+    const loadImage = image => {
+      return new Promise((resolve, reject) => {
+        const loadImg = new Image()
+        loadImg.src = image.urls.regular
+        // wait 2 seconds to simulate loading time
+        loadImg.onload = () => resolve(image.urls.regular)
+        loadImg.onerror = err => reject(err)
+      })
     }
-  }, [status]);
 
-  if (imageArray) {
-    return (
-        <StyledBGContainer
-          viewTime={15}
-          delay={2}
-          imageArray={data.unsplashData}
-          numOfImg={data.unsplashData.length}
-        >
-          {imageArray}
-        </StyledBGContainer>
-      );
-  } else {
-      return <StyledLoadingScreen> Getting Weather </StyledLoadingScreen>
+    Promise.all(data.unsplashData.map(image => loadImage(image)))
+      .then(() => setImgsLoaded(true))
+      .catch(err => console.log("Failed to load images", err))
+  }, [])
+
+
+
+
+  function arrangeImage(array) {
+    let imageEle = array.map((item) => (
+      <img key={item.id} src={item.urls.regular}></img>
+    ));
+    props.loadingImages();
+    return imageEle;
   }
-}
 
-function arrangeImage(array) {
-  let imageEle = array.map((item) => (
-    <img key={item.id} src={item.urls.regular}></img>
-  ));
-  
-  return imageEle;
+  return (
+    <>
+      {imgsLoaded ? 
+      <StyledBGContainer
+        viewTime={15}
+        delay={2}
+        imageArray={data.unsplashData}
+        numOfImg={data.unsplashData.length}
+      >
+        {arrangeImage(data.unsplashData)}
+      </StyledBGContainer>
+      : (
+        <StyledLoadingScreen>loading images</StyledLoadingScreen>
+    )}
+      {/* {!imageArray && (
+        <StyledLoadingScreen> loading images</StyledLoadingScreen>
+      )} */}
+    </>
+  );
 }
 
 const mapStateToProps = (state) => {
-    return {
-      delay: state.settings.delay,
-    };
+  return {
+    delay: state.settings.delay,
   };
-  
-  //step 2
-  const mapDispatchToProps = (dispatch) => {
-    return {
-      loadingImages: () => dispatch(loadingImages(false))
-    };
+};
+
+//step 2
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadingImages: () => dispatch(loadingImages(false)),
   };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(BackgroundSlides);
